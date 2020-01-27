cecd42ec163348a8f250633a2df71336efca0cf7
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-01-27 23:10:59 +0900

    Fix

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ export default () => {
   const [cookies, setCookies, removeCookies] = useCookies(['auth'])
   const classes = useStyles()
 
+  console.log(process.env.REACT_APP_DEV_API_URL)
+
   const handleSignIn = auth => {
     if (!cookies.auth) {
       let expires = new Date()
